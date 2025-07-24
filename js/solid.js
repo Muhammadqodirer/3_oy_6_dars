dc28c46a509297b9c4a3173dc76d6d0614d544ae
MuhammadqodirErgashboyev <pythonfn26@gmail.com>
2025-07-24 10:22:37 +0500

    qoshildi

---
+++ js/solid.js
@@ -0,0 +1,78 @@
+// 1
+
+// function getUserInfo(user) {
+//   return `Foydalanuvchi ismi: ${user.name}, yoshi: ${user.age}`;
+// }
+// console.log(getUserInfo({ name: "Ali", age: 25 }));
+
+
+// 2
+
+// function addTax(product) {
+//   product.priceWithTax = product.price * 1.12;
+//   return product;
+// }
+
+// console.log(addTax({ name: "Telefon", price: 1000 }));
+
+// 4
+
+// function carInfo(car) {
+//   return `Bu ${car.color} rangli ${car.model}, ${car.year}-yil`;
+// }
+// console.log(carInfo({ model: "Cobalt", year: 2022, color: "oq" }));
+
+
+// 5
+
+// function hasEmail(user) {
+//   return user.email ? true : false;
+// }
+// console.log(hasEmail({ name: "Ali", email: "ali@gmail.com" })); 
+// console.log(hasEmail({ name: "Ali" })); 
+
+// 6
+
+// function calculateSpeed(car) {
+//   car.speed = car.distance / car.time;
+//   return car;
+// }
+
+// console.log(calculateSpeed({ model: "Cobalt", distance: 150, time: 3 }));
+
+// 7
+
+// function calculateWorkTime(worker) {
+//   worker.hoursWorked = worker.end - worker.start;
+//   return worker;
+// }
+
+// console.log(calculateWorkTime({ name: "Ali", start: 9, end: 18 }));
+
+// 8
+
+// function calculateVolume(cube) {
+//   cube.volume = cube.side ** 3;
+//   return cube;
+// }
+
+// console.log(calculateVolume({ side: 4 }));
+
+// 9
+
+// function isPasswordStrong(user) {
+//   user.isStrong = user.password.length > 8;
+//   return user;
+// }
+
+// console.log(isPasswordStrong({ username: "test", password: "907786789" }));
+
+// 10
+
+// function calculateBirthYear(person) {
+//   person.birthYear = 2025 - person.age;
+//   return person;
+// }
+
+// console.log(calculateBirthYear({ name: "Laylo", age: 20 }));
+
